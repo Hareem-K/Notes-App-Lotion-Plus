75eb582c45e47f2aef3276fdc7c88f2ee390747a
Hareem Khan <113367388+Hareem-K@users.noreply.github.com>
2024-03-27 22:44:11 -0600

    Update main.py

--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,7 +1,5 @@
 import json
 import boto3
-#from google.oauth2 import id_token
-#from google.auth.transport import requests
 from boto3.dynamodb.conditions import Key
 
 dynamodb_resource = boto3.resource("dynamodb")
@@ -10,20 +8,6 @@ table = dynamodb_resource.Table("lotion-30143555")
 
 def lambda_handler(event, context):
    email = event["queryStringParameters"]["email"]
-   #access_token = event["headers"].get("access_token")
-   #try:
-       #id_info = id_token.verify_oauth2_token(access_token, requests.Request())
-      # if id_info["email"] != email:
-           #raise ValueError("Token email and request email do not match")
-      
-   #except ValueError as e:
-       #return {
-           #"statusCode": 401,
-          # "body": json.dumps({
-               #"message": "Unauthorized: " + str(e)
-          # })
-      # }
-
 
    try:
        response = table.query(
